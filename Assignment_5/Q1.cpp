29ba9a61c7907b0732b58f4664958c27bb10b566
ishaangupta122 <ishaang2209@gmail.com>
2025-10-29 14:58:25 +0530

    added assignment_5 question 1 & 2

--- Assignment_5/Q1.cpp
+++ Assignment_5/Q1.cpp
@@ -1,3 +1,54 @@
 // 1. Implement a C++ program to define three classes Alpha, Beta and Gamma, each class having
 // private data members. Gamma is a class derived from Alpha and Beta (by applying multiple
 // inheritance). Use constructors and destructors to read and display data.
+
+#include <iostream>
+using namespace std;
+
+class Alpha {
+  int a;
+
+ public:
+  Alpha() {
+    cout << "Enter the int value of Alpha: ";
+    cin >> a;
+  }
+  ~Alpha() {
+    cout << "Destructor Alpha Called !\n";
+  }
+};
+
+class Beta {
+  int a;
+
+ public:
+  Beta() {
+    cout << "Enter the int value of Beta: ";
+    cin >> a;
+  }
+  ~Beta() {
+    cout << "Destructor Beta Called !\n";
+  }
+};
+
+class Gamma : public Alpha, public Beta {
+  int a;
+
+ public:
+  Gamma() {
+    cout << "Enter the int value of Gamma: ";
+    cin >> a;
+  }
+  ~Gamma() {
+    cout << "Destructor Gamma Called !\n";
+  }
+};
+
+int main() {
+  cout << "Calling Object of Gamma: " << endl;
+  Gamma obj;
+
+  cout << "Terminating !!!\n";
+
+  return 0;
+}
--- Assignment_5/Q2.cpp
+++ Assignment_5/Q2.cpp
@@ -1,3 +1,73 @@
 // 2. Write a program to define class X, Y and Z. Each class contains one character array as a data
 // member. Using multiple inheritance, concatenate strings of class X and Y and store it in class Z.
 // Using constructor and destructors, show all the three strings.
+
+#include <iostream>
+using namespace std;
+
+class X {
+ protected:
+  char str1[50];
+
+ public:
+  X() {
+    cout << "Enter first string: ";
+    cin >> str1;
+  }
+  ~X() {
+    cout << "Destructor of X called.\n";
+  }
+};
+
+class Y {
+ protected:
+  char str2[50];
+
+ public:
+  Y() {
+    cout << "Enter second string: ";
+    cin >> str2;
+  }
+  ~Y() {
+    cout << "Destructor of Y called.\n";
+  }
+};
+
+class Z : public X, public Y {
+  char str3[100];
+
+ public:
+  Z() {
+    int i = 0, j = 0;
+
+    while (str1[i] != '\0') {
+      str3[i] = str1[i];
+      i++;
+    }
+
+    while (str2[j] != '\0') {
+      str3[i] = str2[j];
+      i++;
+      j++;
+    }
+
+    cout << "\nConcatenation Done!\n";
+  }
+
+  void show() {
+    cout << "\nString 1: " << str1;
+    cout << "\nString 2: " << str2;
+    cout << "\nConcatenated String: " << str3 << endl;
+  }
+
+  ~Z() {
+    cout << "Destructor of Z called.\n";
+  }
+};
+
+int main() {
+  Z obj;
+  obj.show();
+
+  return 0;
+}
