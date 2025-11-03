537e5355718626285cde9e4e2d87147ba596e6e7
ishaangupta122 <ishaang2209@gmail.com>
2025-11-03 21:54:36 +0530

    completed assignment 5

--- Assignment_5/Q3.cpp
+++ Assignment_5/Q3.cpp
@@ -1,2 +1,48 @@
 // 3. Write a C++ program to implement the diamond problem (hybrid inheritance). State the necessary
 // assumptions using comments. – (HINT: use virtual base class)
+
+#include <iostream>
+using namespace std;
+
+class A {
+ public:
+  int value;
+  A() {
+    cout << "Constructor of A called" << endl;
+    value = 10;
+  }
+};
+
+class B : virtual public A {
+ public:
+  B() {
+    cout << "Constructor of B called" << endl;
+  }
+};
+
+class C : virtual public A {
+ public:
+  C() {
+    cout << "Constructor of C called" << endl;
+  }
+};
+
+class D : public B, public C {
+ public:
+  D() {
+    cout << "Constructor of D called" << endl;
+  }
+
+  void show() {
+    cout << "Value from class A = " << value << endl;
+  }
+};
+
+int main() {
+  cout << "Performing Hybrid Inheritance: \n\n";
+
+  D obj;
+  obj.show();
+
+  return 0;
+}
--- Assignment_5/Q4.cpp
+++ Assignment_5/Q4.cpp
@@ -1,3 +1,105 @@
 // 4. Write a C++ program creating an abstract class Student. Create three derived classes Science,
 // Art and Commerce from the base class. Create the objects of the derived classes and process them
 // and access them using array of pointer of type Student.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+ protected:
+  string name;
+  int rollNo;
+
+ public:
+  Student(string n, int r) {
+    name = n;
+    rollNo = r;
+  }
+
+  virtual void showDetails() = 0;
+
+  virtual ~Student() {}
+};
+
+class Science : public Student {
+  float physics, chemistry, maths;
+
+ public:
+  Science(string n, int r, float p, float c, float m) : Student(n, r) {
+    physics = p;
+    chemistry = c;
+    maths = m;
+  }
+
+  void showDetails() override {
+    cout << "\n---- Science Student ----" << endl;
+    cout << "Name: " << name << endl;
+    cout << "Roll No: " << rollNo << endl;
+    cout << "\nMarks of Student:" << endl;
+    cout << "Physics: " << physics << endl;
+    cout << "Chemistry: " << chemistry << endl;
+    cout << "Mathematics: " << maths << endl;
+    cout << "-------------------------" << endl;
+  }
+};
+
+class Art : public Student {
+  float history, literature;
+
+ public:
+  Art(string n, int r, float h, float l) : Student(n, r) {
+    history = h;
+    literature = l;
+  }
+
+  void showDetails() override {
+    cout << "\n---- Art Student ----" << endl;
+    cout << "Name: " << name << endl;
+    cout << "Roll No: " << rollNo << endl;
+    cout << "\nMarks of Student:" << endl;
+    cout << "History: " << history << endl;
+    cout << "Literature: " << literature << endl;
+    cout << "---------------------" << endl;
+  }
+};
+
+class Commerce : public Student {
+  float accounts, economics, businessStudies;
+
+ public:
+  Commerce(string n, int r, float a, float e, float b) : Student(n, r) {
+    accounts = a;
+    economics = e;
+    businessStudies = b;
+  }
+
+  void showDetails() override {
+    cout << "\n---- Commerce Student ----" << endl;
+    cout << "Name: " << name << endl;
+    cout << "Roll No: " << rollNo << endl;
+    cout << "\nMarks of Student:" << endl;
+    cout << "Accounts : " << accounts << endl;
+    cout << "Economics: " << economics << endl;
+    cout << "Business Studies: " << businessStudies << endl;
+    cout << "--------------------------" << endl;
+  }
+};
+
+int main() {
+  Student* students[3];
+
+  students[0] = new Science("Aarav", 101, 85, 90, 88);
+  students[1] = new Art("Palak", 102, 80, 92);
+  students[2] = new Commerce("Rohan", 103, 75, 89, 84);
+
+  for (int i = 0; i < 3; i++) {
+    students[i]->showDetails();
+  }
+
+  for (int i = 0; i < 3; i++) {
+    delete students[i];
+  }
+
+  return 0;
+}
